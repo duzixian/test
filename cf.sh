90d4e7935b87f06cea5cfe9db07cfc01980e158e
duzixian <2693171261@qq.com>
2025-08-31 00:57:42 +0800

    Update cf.sh

--- cf.sh
+++ cf.sh
@@ -1,10 +1,13 @@
-cf_install() {
+cf_int() {
 curl -fsSL -o cf https://ghfast.top/https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64
 chmod +x cf
 }
 
-
 cf_url() {
     local file_name=${1:-log.txt}          # 缺省文件名 log.txt
     grep -oE 'https://[^[:space:]]*\.trycloudflare\.com' "$file_name" | head -n1
+}
+
+sshd2222() {
+    /usr/sbin/sshd -D -p 2222 -o PermitRootLogin=yes &
 }
